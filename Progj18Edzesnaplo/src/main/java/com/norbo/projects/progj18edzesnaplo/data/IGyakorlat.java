5e61659b3a98c516e6dac5bfdb092cc78a550c9a
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-17 12:57:07 +0100

    webről jsonból átalakított gyakorlat lista

--- Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/data/IGyakorlat.java
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/data/IGyakorlat.java
@@ -1,14 +1,27 @@
 package com.norbo.projects.progj18edzesnaplo.data;
 
+import java.io.Serializable;
+import org.json.JSONObject;
+
 /**
  *  egy Gyakorlat typust reprezentáló interface
  * @author igloi
  */
-public interface IGyakorlat {
+public interface IGyakorlat extends Serializable {
     public int getId();
-    public String getNew();
+    public String getMegnevezes();
     public Izomcsoport getIzomcsoport();
     public String getLeiras();
-    public String getVideoId();
-    public int getVideoPoz();
+    public String getVideolink();
+    public int getVideostartpoz();
+    
+    public void setId(int id);
+    public void setMegnevezes(String megnevezes);
+    public void setIzomcsoport(Izomcsoport izomcsop);
+    public void setLeiras(String leiras);
+    public void setVideolink(String videolink);
+    public void setVideostartpoz(int videostartpoz);
+    
+    public JSONObject readObject(String source);
+    public void loadGyakorlatFromJson(JSONObject jsonobject);
 }
