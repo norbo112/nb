6327567feea4fdfc38809e58d85d9ce2c63a46bc
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-17 12:57:00 +0100

    webről jsonból átalakított gyakorlat lista

--- Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/GyakorlatLista.java
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/GyakorlatLista.java
@@ -9,14 +9,28 @@ import com.norbo.projects.progj18edzesnaplo.data.Gyakorlat;
 import com.norbo.projects.progj18edzesnaplo.data.IGyakorlat;
 import com.norbo.projects.progj18edzesnaplo.data.Izomcsoport;
 import com.norbo.projects.progj18edzesnaplo.example.MasikGyakImpl;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  *
  * @author igloi
  */
 public class GyakorlatLista {
+    public static final String GYAKURL = "http://www.edzesnaplo.eu/android/naplolistause.php";
     public static List<IGyakorlat> getGyakorlatList() {
         List<IGyakorlat> result = new ArrayList<>();
         
@@ -41,5 +55,62 @@ public class GyakorlatLista {
         result.add(new MasikGyakImpl(5, "Kétkezes evezés", Izomcsoport.HAT, "Pucsít", "", 0));
         
         return result;
+    }
+    
+    public static List<IGyakorlat> getGyakorlatList(String url) throws MalformedURLException, IOException {
+        List<IGyakorlat> gyakorlats = new ArrayList<>();
+        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        
+        String params = "keres=GYAKLISTALEKER";
+        
+        try (OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream())) {
+            out.write(params);
+        }
+        
+        if(con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+            String inputstring = getStringFromInput(con.getInputStream());
+//            System.out.println(inputstring);
+            JSONObject job = new JSONObject(inputstring);
+
+            if(getOpt(job, "hgyakorlatlista").isPresent()) {
+                System.out.println("Hiba történt az adatok fogadása közben: "+
+                        getOpt(job,"hgyakorlatlista").get());
+                return null;
+            }
+
+            JSONArray josarr = job.getJSONArray("gyakorlatlista");
+            for (int i = 0; i < josarr.length(); i++) {
+                Gyakorlat gyaksi = new Gyakorlat();
+                gyaksi.loadGyakorlatFromJson(josarr.getJSONObject(i));
+                gyakorlats.add(gyaksi);
+            } 
+        } else {
+            System.out.println("Nem lehetett elérni az adatokat a szerverről");
+        }
+        
+        return gyakorlats;
+    }
+    
+    private static String getStringFromInput(InputStream in) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+        String line;
+        while((line = bin.readLine()) != null) {
+            sb.append(line);
+        }
+        
+        return sb.toString();
+    }
+    
+    private static Optional<String> getOpt(JSONObject obj, String key) {
+        Optional<String> res = Optional.empty();
+        try {
+            res = Optional.ofNullable(obj.getString(key));
+        } catch (JSONException e) {
+            System.out.println("Hiba történt: loggolni való lesz: "+e.getMessage());
+        }
+        return res;
     }
 }
