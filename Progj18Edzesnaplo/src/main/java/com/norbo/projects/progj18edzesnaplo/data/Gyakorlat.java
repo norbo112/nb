771431f20483d64952dbf29e27342156ddda2fc1
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-17 12:57:18 +0100

    webről jsonból átalakított gyakorlat lista

--- Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/data/Gyakorlat.java
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/data/Gyakorlat.java
@@ -5,12 +5,17 @@
  */
 package com.norbo.projects.progj18edzesnaplo.data;
 
+import org.json.JSONObject;
+
 /**
  *
  * @author igloi
  */
 public class Gyakorlat extends AbstractGyakorlat {
 
+    public Gyakorlat() {
+    }
+    
     public Gyakorlat(int id, String nev, 
             Izomcsoport izomcsoport, 
             String leiras, 
@@ -18,5 +23,24 @@ public class Gyakorlat extends AbstractGyakorlat {
             int videoPoz) {
         super(id, nev, izomcsoport, leiras, videoId, videoPoz);
     }
-    
+
+    @Override
+    public JSONObject readObject(String source) {
+        return new JSONObject(source);
+    }
+
+    @Override
+    public void loadGyakorlatFromJson(JSONObject jsonobject) {
+        setId(jsonobject.getInt("gyak_id"));
+        setMegnevezes(jsonobject.getString("megnevezes"));
+        setIzomcsoport(Izomcsoport.getIzomCsoport(jsonobject.getString("csoport")));
+        setLeiras(jsonobject.getString("leiras"));
+        setVideolink(jsonobject.getString("videolink"));
+        setVideostartpoz(jsonobject.getInt("videostartpoz"));
+    }
+
+    @Override
+    public int compareTo(IGyakorlat o) {
+        return getIzomcsoport().compareTo(o.getIzomcsoport());
+    }
 }
