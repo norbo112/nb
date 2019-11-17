e85258b30f95d47801886bf623a40dbd9f71d191
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-17 19:38:44 +0100

    adatok átalakítása jsonból vagy csvből

---
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/AbstractTrasform.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.norbo.projects.progj18edzesnaplo.dodata;
+
+import com.norbo.projects.progj18edzesnaplo.data.IGyakorlat;
+import java.util.List;
+
+/**
+ *
+ * @author igloi
+ * @param <T>
+ */
+public abstract class AbstractTrasform<T> implements IGyTramsform<T> {
+    private final String url;
+
+    public AbstractTrasform(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    } 
+    
+    @Override
+    public abstract List<IGyakorlat> betolt(T source);
+}
