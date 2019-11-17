c1c6bf147568a08f093ad0a88777f8175f5992d5
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-17 12:56:29 +0100

    egyébb módosítások

--- Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/main/Main.java
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/main/Main.java
@@ -2,18 +2,32 @@ package com.norbo.projects.progj18edzesnaplo.main;
 
 import com.norbo.projects.progj18edzesnaplo.data.IGyakorlat;
 import com.norbo.projects.progj18edzesnaplo.dodata.GyakorlatLista;
+import com.norbo.projects.progj18edzesnaplo.gui.gyakorlatok.Gyakorlatok;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  *
  * @author igloi
  */
 public class Main {
+    private static final Comparator<IGyakorlat> COMP_IZOMCSOP = 
+            (o1,o2)-> o1.getIzomcsoport().compareTo(o2.getIzomcsoport());
+    
     public static void main(String[] args) {
-        List<IGyakorlat> gy = GyakorlatLista.getGyakorlatList();
-        
-        for(IGyakorlat igy: gy) {
-            System.out.println(igy);
+        try {
+            List<IGyakorlat> gyfromurl = GyakorlatLista.getGyakorlatList(GyakorlatLista.GYAKURL);
+//            for(IGyakorlat g: gyfromurl) {
+//                System.out.println(g);
+//            }
+            gyfromurl.sort(COMP_IZOMCSOP);
+            new Gyakorlatok(gyfromurl).showFrame();
+        } catch (IOException ex) {
+            System.out.println("Hiba törént az adatok elérése közben: "+ex);
         }
+        
     }
 }
