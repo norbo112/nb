58e02ace0abfe8c09367b8ed4645e56cce6ac303
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-18 20:47:32 +0100

    getStringFromFile, megvalósítása, legalábbis ha fájl elérést kap

--- Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/CsvGyTranform.java
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/CsvGyTranform.java
@@ -8,24 +8,47 @@ package com.norbo.projects.progj18edzesnaplo.dodata;
 import com.norbo.projects.progj18edzesnaplo.data.Gyakorlat;
 import com.norbo.projects.progj18edzesnaplo.data.IGyakorlat;
 import com.norbo.projects.progj18edzesnaplo.data.Izomcsoport;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author igloi
  */
 public class CsvGyTranform extends AbstractTrasform<String>{
+
+    @Override
+    String getStringFromFile(String path) {
+        StringBuilder sb = new StringBuilder();
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(
+                new FileInputStream(path)))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(CsvGyTranform.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return sb.toString();
+    }
     
     @Override
     public List<IGyakorlat> betolt(String source) {
+        source = getStringFromFile(source);
         List<IGyakorlat> gyakorlats = new ArrayList<>();
         String[] sorok = source.split("\n");
         for (String string : sorok) {
             String[] egysor = string.split(";",7);
             gyakorlats.add(new Gyakorlat(Integer.parseInt(egysor[0]), egysor[2],
                     Izomcsoport.getIzomCsoport(egysor[1]), egysor[3], egysor[4],
-                    Integer.parseInt(egysor[5])));
+                    egysor[5].length() != 0 ? Integer.parseInt(egysor[5]) : 0 ));
         }
         
         return gyakorlats;
