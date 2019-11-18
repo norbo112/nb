cdefdcdfef6e4bb3912a1c29ad286deaf7cc4bc8
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-18 18:03:27 +0100

    modositás

--- Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/GyakorlatBetolto.java
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/GyakorlatBetolto.java
@@ -6,14 +6,7 @@
 package com.norbo.projects.progj18edzesnaplo.dodata;
 
 import com.norbo.projects.progj18edzesnaplo.data.IGyakorlat;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
