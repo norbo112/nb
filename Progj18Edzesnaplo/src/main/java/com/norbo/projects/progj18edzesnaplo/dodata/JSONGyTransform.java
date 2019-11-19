aafdd76b500c77e8241391ec5ac93b378e7ec824
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-19 17:33:57 +0100

    változás

--- Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/JSONGyTransform.java
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/JSONGyTransform.java
@@ -8,8 +8,11 @@ package com.norbo.projects.progj18edzesnaplo.dodata;
 import com.norbo.projects.progj18edzesnaplo.data.Gyakorlat;
 import com.norbo.projects.progj18edzesnaplo.data.IGyakorlat;
 import com.norbo.projects.progj18edzesnaplo.data.Izomcsoport;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -17,28 +20,32 @@ import org.json.JSONObject;
  *
  * @author igloi
  */
-public class JSONGyTransform extends AbstractTrasform<JSONObject> {
-
-    @Override
-    String getStringFromFile(String path) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+public class JSONGyTransform extends AbstractTrasform { 
     
     @Override
-    public List<IGyakorlat> betolt(JSONObject source) {
+    public List<IGyakorlat> betolt(String path) {
         List<IGyakorlat> gyakorlats = new ArrayList<>();
        
-        JSONArray josarr = source.getJSONArray("gyakorlatlista");
-        for (int i = 0; i < josarr.length(); i++) {
-            JSONObject jobi = josarr.getJSONObject(i);
-            gyakorlats.add(new Gyakorlat(
-                    jobi.getInt("gyak_id"),
-                    jobi.getString("megnevezes"),
-                    Izomcsoport.getIzomCsoport(jobi.getString("csoport")),
-                    jobi.get("leiras").toString(),
-                    jobi.getString("videolink"),
-                    jobi.getInt("videostartpoz")
-            ));
+        JSONObject source = null;
+        try {
+            source = new JSONObject(getString(path, KERES_GYAKLEKER));
+        } catch (IOException ex) {
+            Logger.getLogger(JSONGyTransform.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        if(source != null) {
+            JSONArray josarr = source.getJSONArray("gyakorlatlista");
+            for (int i = 0; i < josarr.length(); i++) {
+                JSONObject jobi = josarr.getJSONObject(i);
+                gyakorlats.add(new Gyakorlat(
+                        jobi.getInt("gyak_id"),
+                        jobi.getString("megnevezes"),
+                        Izomcsoport.getIzomCsoport(jobi.getString("csoport")),
+                        jobi.get("leiras").toString(),
+                        jobi.getString("videolink"),
+                        jobi.getInt("videostartpoz")
+                ));
+            }
         }
         
         return gyakorlats;
