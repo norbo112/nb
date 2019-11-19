79000a456d15bae82f4bc9e4d8be2288aa5bd609
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-19 19:51:36 +0100

    kiegészítés mentési dolgokkal

--- Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/ExcelGyTransform.java
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/ExcelGyTransform.java
@@ -10,6 +10,8 @@ import com.norbo.projects.progj18edzesnaplo.data.IGyakorlat;
 import com.norbo.projects.progj18edzesnaplo.data.Izomcsoport;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -17,6 +19,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -26,20 +29,13 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory;
  *
  * @author igloi
  */
-public class ExcelGyTransform extends AbstractTrasform<String>{
-
+public class ExcelGyTransform extends AbstractTransform {
     @Override
-    String getStringFromFile(String path) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-
-    @Override
-    public List<IGyakorlat> betolt(String source) {
+    public List<IGyakorlat> betolt(String path) {
         List<IGyakorlat> gyakorlats = new ArrayList<>();
         try {
             
-            InputStream in = new FileInputStream(new File(source));
+            InputStream in = new FileInputStream(new File(path));
             
             XSSFWorkbook workbook = XSSFWorkbookFactory.createWorkbook(in);
             XSSFSheet sheet = workbook.getSheetAt(0);
@@ -72,5 +68,41 @@ public class ExcelGyTransform extends AbstractTrasform<String>{
             Logger.getLogger(ExcelGyTransform.class.getName()).log(Level.SEVERE, null, ex);
         }
         return gyakorlats;
+    }
+
+    @Override
+    public boolean gyakorlatMent(List<IGyakorlat> gyks, String path) {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("gyakorlatlista");
+        
+        XSSFRow row = sheet.createRow(0);
+        row.createCell(0).setCellValue("ID");
+        row.createCell(1).setCellValue("Izomcsoport");
+        row.createCell(2).setCellValue("Megnevezés");
+        row.createCell(3).setCellValue("Leírás");
+        row.createCell(4).setCellValue("Videolink");
+        row.createCell(5).setCellValue("Videostartpoz");
+        
+        for (int i = 1; i < gyks.size(); i++) {
+            row = sheet.createRow(i);
+            row.createCell(0).setCellValue((double)gyks.get(i).getId());
+            row.createCell(1).setCellValue(gyks.get(i).getIzomcsoport().toString());
+            row.createCell(2).setCellValue(gyks.get(i).getMegnevezes());
+            row.createCell(3).setCellValue(gyks.get(i).getLeiras());
+            row.createCell(4).setCellValue(gyks.get(i).getVideolink());
+            row.createCell(5).setCellValue((double)gyks.get(i).getVideostartpoz());
+        }
+        
+        try(FileOutputStream out = new FileOutputStream(path)) {
+            workbook.write(out);
+            
+            return true;
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ExcelGyTransform.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ExcelGyTransform.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
     }
 }
