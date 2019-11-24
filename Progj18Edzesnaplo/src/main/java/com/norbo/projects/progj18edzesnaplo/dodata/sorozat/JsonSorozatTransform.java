5523ca2f536883573be80102f8e403f2ed875a75
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-24 23:18:31 +0100

    jsonba való mentéshez és betöltéshez

--- Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/sorozat/JsonSorozatTransform.java
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/sorozat/JsonSorozatTransform.java
@@ -5,11 +5,18 @@
  */
 package com.norbo.projects.progj18edzesnaplo.dodata.sorozat;
 
+import com.norbo.projects.progj18edzesnaplo.data.Gyakorlat;
+import com.norbo.projects.progj18edzesnaplo.data.Izomcsoport;
 import com.norbo.projects.progj18edzesnaplo.data.naplo.Sorozat;
 import com.norbo.projects.progj18edzesnaplo.data.naplo.SorozatInterface;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -45,5 +52,69 @@ public class JsonSorozatTransform implements SorozatTransform<String> {
         }
         return false;
     }
+
+    @Override
+    public List<SorozatInterface> betolt(String path) {
+        List<SorozatInterface> sorozat = new ArrayList<>();
+        
+        String source = null;
+        StringBuilder sb = new StringBuilder();
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            
+            source = sb.toString();
+        } catch (IOException ex) {
+            Logger.getLogger(JsonSorozatTransform.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        if(source != null) {
+            JSONObject job = new JSONObject(source);
+            JSONArray arr = job.getJSONArray("edzesnap");
+
+            for(int i=0; i<arr.getJSONArray(0).length(); i++) {
+                JSONObject ijob = arr.getJSONArray(0).getJSONObject(i);
+                
+                JSONObject gyakjob = ijob.getJSONObject("gyakorlat");
+                
+                Gyakorlat gy = new Gyakorlat();
+                gy.setId(gyakjob.getInt("id"));
+                gy.setIzomcsoport(Izomcsoport.getCsoportFromJSON(gyakjob.getString("izomcsoport")));
+                gy.setMegnevezes(gyakjob.getString("megnevezes"));
+                gy.setLeiras(gyakjob.getString("leiras"));
+                gy.setVideolink(gyakjob.getString("videolink"));
+                gy.setVideostartpoz(gyakjob.getInt("videostartpoz"));
+                Sorozat s = new Sorozat(gy);
+                s.setGyakRogzitesIdopontja(LocalDateTime.parse(ijob.getString("gyakRogzitesIdopontja")));
+                
+                List<Integer> list = getIntList(ijob.getJSONArray("sulyList"));
+                s.setSulyList(list);
+                list = getIntList(ijob.getJSONArray("ismList"));
+                s.setIsmList(list);
+                List<LocalTime> times = getTimeList(ijob.getJSONArray("ismIdoList"));
+                s.setIsmIdoList(times);
+                sorozat.add(s);
+            }
+        }
+        
+        return sorozat;
+    }
     
+    private List<Integer> getIntList(JSONArray arr) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < arr.length(); i++) {
+            result.add(arr.getInt(i));
+        }
+        return result;
+    }
+    
+    private List<LocalTime> getTimeList(JSONArray arr) {
+        List<LocalTime> result = new ArrayList<>();
+        for (int i = 0; i < arr.length(); i++) {
+            result.add(LocalTime.parse(arr.getString(i)));
+        }
+        return result;
+    }
 }
