31b046d665afae954819196e2add78474bea9def
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-24 10:44:52 +0100

    sorozat mentése jsonba

---
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/sorozat/JsonSorozatTransform.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.norbo.projects.progj18edzesnaplo.dodata.sorozat;
+
+import com.norbo.projects.progj18edzesnaplo.data.naplo.Sorozat;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author igloi
+ */
+public class JsonSorozatTransform implements SorozatTransform<String> {
+
+    /**
+     * Sorozatok kimentése JSon formátumba
+     * @param sorozats sorozatok mint paraméter
+     * @param path fájl útvonala
+     * @return true ha sikeres volt a fájl mentés, false ha nem
+     */
+    @Override
+    public boolean ment(List<Sorozat> sorozats, String path) {
+        JSONObject obj = new JSONObject(sorozats);
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            obj.write(bw);
+            return true;
+        } catch (IOException ex) {
+            Logger.getLogger(JsonSorozatTransform.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
+}
