ecaabc3c859d041dc5b20ff055ab5fbd19ac4a9b
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2019-11-26 19:55:11 +0100

    kisebb javítás

--- Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/sorozat/JsonSorozatTransform.java
+++ Progj18Edzesnaplo/src/main/java/com/norbo/projects/progj18edzesnaplo/dodata/sorozat/JsonSorozatTransform.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
